f5bfaad54828bbf24f84ca2792fd88cb8f6e6fc1
Amartya Dutta <56846056+KatameRonin@users.noreply.github.com>
2024-05-03 11:57:51 -0400

    normalized colors

--- songs.js
+++ songs.js
@@ -7,13 +7,13 @@ function drawBarPlotWithPlotly(data) {
         return `rgba(${255 * (1 - normalizedPopularity)}, ${255 * normalizedPopularity}, 100, 0.7)`;
     });
 
-    
+
     var trace = {
         x: data.map(d => d.track_name),
         y: data.map(d => d.popularity),
         text: data.map(d => `Artist: ${d.artist_name}<br>Release Date: ${d.album_release_date}`),
         type: 'bar',
-        marker: {color: '#69b3a2'}
+        marker: {color: colors}
     };
 
     var layout = {
@@ -29,7 +29,7 @@ function drawBarPlotWithPlotly(data) {
         margin: {t: 30, r: 30, b: 100, l: 100},
         hovermode: 'closest',
         bargap: 0.1, // Increase the gap between bars if needed
-        width: 1400, // Increased width
+        width: 1200, // Increased width
         height: 600 // Increased height
     };
 
